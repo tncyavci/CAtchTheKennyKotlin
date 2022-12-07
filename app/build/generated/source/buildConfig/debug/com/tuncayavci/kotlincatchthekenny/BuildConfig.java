0a444f0538cb8f6ea2a4ffbd89b8e0e95587e363
Tuncay Avcı <tncyavci@gmail.com>
2022-12-07 23:42:54 +0300

    commit

---
+++ app/build/generated/source/buildConfig/debug/com/tuncayavci/kotlincatchthekenny/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.tuncayavci.kotlincatchthekenny;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.tuncayavci.kotlincatchthekenny";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
